629b78ee1501fc6d320f9002434cffb7fcca0784
Jason Barto <jason.p.barto@gmail.com>
2019-01-17 08:27:56 +0000

    removed users from part 1

--- part1/report_entities.py
+++ part1/report_entities.py
@@ -6,38 +6,6 @@ policy_arn = 'arn:aws:iam::776347453069:policy/ManagedPolicy'
 print ("Searching for entities that do NOT have the following policy attached:")
 print (policy_arn)
 
-def get_usernames (marker = None):
-    users = []
-    if marker is None:
-        users_resp = iam.list_users (PathPrefix = '/', MaxItems = 100)
-    else:
-        users_resp = iam.list_users (PathPrefix = '/', MaxItems = 100, Marker = marker)
-        marker = None
-
-    for user in users_resp['Users']:
-        users.append (user['UserName'])
-
-    if 'IsTruncated' in users_resp and users_resp['IsTruncated']:
-        marker = users_resp['Marker']
-
-    return (users, marker)
-
-def get_user_policies (username, marker = None):
-    policies = []
-    if marker is None:
-        policy_resp = iam.list_attached_user_policies (UserName = username, MaxItems = 100)
-    else:
-        policy_resp = iam.list_attached_user_policies (UserName = username, MaxItems = 100, Marker = marker)
-        marker = None
-
-    for policy in policy_resp['AttachedPolicies']:
-        policies.append (policy['PolicyArn'])
-
-    if 'IsTruncated' in policy_resp and policy_resp['IsTruncated']:
-        marker = policy_resp['Marker']
-
-    return (policies, marker)
-
 def get_roles (marker = None):
     roles = []
     if marker is None:
@@ -69,24 +37,6 @@ def get_role_policies (rolename, marker = None):
         marker = policy_resp['Marker']
 
     return (policies, marker)
-
-print ("List users without policy attached")
-(iam_users, marker) = get_usernames ()
-while marker is not None:
-    (users, marker) = get_usernames (marker = marker)
-    iam_users += users
-print ("Checking users: {}".format (iam_users))
-
-for username in iam_users:
-    (user_policies, marker) = get_user_policies (username)
-    while marker is not None:
-        (policies, marker) = get_user_policies (username, marker = marker)
-        user_policies += policies
-
-    if policy_arn in user_policies:
-        print ("User {} is compliant".format (username))
-    else:
-        print ("User {} is NOT compliant".format (username))
 
 print ("List roles without policy attached")
 (iam_roles, marker) = get_roles ()
