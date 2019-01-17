39f27fc43ec48d995e03e6958a0f98c2c58614b4
Jason Barto <jason.p.barto@gmail.com>
2019-01-17 11:27:07 +0000

    added incremental interim stage

---
+++ part3/src/report_entities.py
@@ -0,0 +1,76 @@
+import boto3
+import json
+import os
+
+iam = boto3.client ('iam')
+sns = boto3.client ('sns')
+
+topic_arn = os.environ['topic_arn']
+policy_arn = 'arn:aws:iam::776347453069:policy/ManagedPolicy'
+
+print ("Searching for entities that do NOT have the following policy attached:")
+print (policy_arn)
+
+def get_roles (marker = None):
+    roles = []
+    if marker is None:
+        roles_resp = iam.list_roles (PathPrefix = '/', MaxItems = 100)
+    else:
+        roles_resp = iam.list_roles (PathPrefix = '/', MaxItems = 100, Marker = marker)
+        marker = None
+
+    for role in roles_resp['Roles']:
+        roles.append (role['RoleName'])
+
+    if 'IsTruncated' in roles_resp and roles_resp['IsTruncated']:
+        marker = roles_resp['Marker']
+
+    return (roles, marker)
+
+def get_role_policies (rolename, marker = None):
+    policies = []
+    if marker is None:
+        policy_resp = iam.list_attached_role_policies (RoleName = rolename, MaxItems = 100)
+    else:
+        policy_resp = iam.list_attached_role_policies (RoleName = rolename, MaxItems = 100, Marker = marker)
+        marker = None
+
+    for policy in policy_resp['AttachedPolicies']:
+        policies.append (policy['PolicyArn'])
+
+    if 'IsTruncated' in policy_resp and policy_resp['IsTruncated']:
+        marker = policy_resp['Marker']
+
+    return (policies, marker)
+
+def handler (event, context):
+    non_compliant_roles = []
+    print ("List roles without policy attached")
+    (iam_roles, marker) = get_roles ()
+    while marker is not None:
+        (roles, marker) = get_roles (marker = marker)
+        iam_roles += roles
+    print ("Checking {} roles".format (len(iam_roles)))
+
+    for rolename in iam_roles:
+        (role_policies, marker) = get_role_policies (rolename)
+        while marker is not None:
+            (policies, marker) = get_role_policies (rolename, marker = marker)
+            role_policies += policies
+
+        if policy_arn in role_policies:
+            print ("User {} is compliant".format (rolename))
+        else:
+            print ("Role {} is NOT compliant".format (rolename))
+            non_compliant_roles.append (rolename)
+
+    report = {
+        'NonCompliantRoles': non_compliant_roles
+    }
+
+    sns.publish (TopicArn = topic_arn, Subject = 'IAM Compliance report', Message = json.dumps (report))
+
+    return report
+
+if __name__ == '__main__':
+    print (handler (None, None))
---
+++ part3/terraform.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_sns_topic" "entity_report_topic" {
+  name = "entity-report-topic"
+}
+
+data "archive_file" "lambda_zip" {
+    type        = "zip"
+    source_dir  = "src"
+    output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "entity_report_function" {
+  filename = "lambda.zip"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  function_name = "EntityComplianceReport"
+  role = "${aws_iam_role.entity_report_role.arn}"
+  description = "Check that all roles have managed IAM policies applied."
+  handler = "report_entities.handler"
+  runtime = "python3.6"
+  timeout = 60
+  environment = {
+      variables = {
+          topic_arn = "${aws_sns_topic.entity_report_topic.arn}"
+      }
+  }
+}
+
+resource "aws_iam_role" "entity_report_role" {
+  name = "EntityComplianceReportRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "entity_report_policy" {
+  name       = "EntityComplianceReportPolicy"
+  role       = "${aws_iam_role.entity_report_role.name}"
+  depends_on = ["aws_iam_role.entity_report_role"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "sns:Publish",
+                "iam:ListAttachedRolePolicies",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:log-group:*",
+                "arn:aws:iam::*:role/*",
+                "arn:aws:sns:*:*:*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "config:ListDiscoveredResources",
+                "iam:ListRoles",
+                "config:PutEvaluations",
+                "logs:CreateLogGroup"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "logs:PutLogEvents",
+            "Resource": "arn:aws:logs:*:*:log-group:*:*:*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_rule" "run_entity_compliance_report" {
+  name        = "RunEntityComplianceReport"
+  description = "Report nightly on entity compliance"
+  schedule_expression = "cron(0 2 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda" {
+  rule      = "${aws_cloudwatch_event_rule.run_entity_compliance_report.name}"
+  arn       = "${aws_lambda_function.entity_report_function.arn}"
+}
